df598d6d6bd9aba48c64bca4d3b10af59f8d0121
uni51 <unieye51@gmail.com>
2023-10-16 14:18:43 +0900

    Footerの作成と、それに伴う画面遷移の対応

---
+++ lib/list_chat.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ListChat extends StatelessWidget {
+  const ListChat({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(child: Text('お知らせ')),
+      color: Colors.red[200],
+    );
+  }
+}
